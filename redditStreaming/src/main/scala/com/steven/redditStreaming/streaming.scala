dfa1f3db23a3a727568d94c6a393f0023f581306
steven hurwitt <stevenhurwitt@gmail.com>
2022-06-30 16:37:16 -0500

    commit

--- redditStreaming/src/main/scala/com/steven/redditStreaming/streaming.scala
+++ redditStreaming/src/main/scala/com/steven/redditStreaming/streaming.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql._
 import org.apache.spark.sql.functions._ 
 import org.apache.spark.sql.types._ 
 
-object twitter {
+object streaming {
 
 def streaming(args:Array[String]):Unit= {
 
