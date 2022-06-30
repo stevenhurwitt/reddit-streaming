d6341e5283731be2a2a6f3e33d0d69f43b12afe4
steven hurwitt <stevenhurwitt@gmail.com>
2022-06-30 10:39:25 -0500

    update spark streaming

--- redditStreaming/src/main/scala/com/steven/redditStreaming/streaming.scala
+++ redditStreaming/src/main/scala/com/steven/redditStreaming/streaming.scala
@@ -7,8 +7,8 @@ object streaming {
 def main(args:Array[String]):Unit= {
 
 val spark: SparkSession = SparkSession.builder()
-      // .master("spark://xanaxprincess.asuscomm.com:7077")
-      .master("spark://spark-master:7077")
+      .master("spark://xanaxprincess.asuscomm.com:7077")
+      // .master("spark://spark-master:7077")
       // .master("spark://192.168.50.7:7077")
       .appName("kafkaProducer")
       .getOrCreate()
@@ -17,8 +17,8 @@ import spark.implicits._
 
 val kafka_df = spark.readStream
         .format("kafka")
-        .option("kafka.bootstrap.servers", "kafka:9092")
-        .option("subscribe", "reddit")
+        .option("kafka.bootstrap.servers", "xanaxprincess.asuscomm.com:9092")
+        .option("subscribe", "twitter")
         .option("includeHeaders", "true")
         .load()
 
@@ -48,11 +48,25 @@ val kafka_df = spark.readStream
     //         .add("metadataVersion", StringType)
     //       , true)
 
+    val json_schema = 
+        new ArrayType(
+          new StructType()
+            .add("", StringType)
+            .add("", StringType)
+            //   .add("struct", new StructType()
+            //     .add("batchId", StringType)
+            //     , true)
+            // , true)
+            // .add("metadataVersion", StringType)
+        )
 
-      // val payload = kafka_df.selectExpr("CAST(body AS STRING) as json", "enqueuedTime", "properties").select(from_json($"json", json_schema).as("data"), col("enqueuedTime"), col("properties"))
 
-      // var eventDF = payload.select(explode(payload("data")).alias("d"))
+      val payload = kafka_df.selectExpr("CAST(body AS STRING) as json", "enqueuedTime", "properties").select(from_json($"json", json_schema).as("data"), col("enqueuedTime"), col("properties"))
 
+      var eventDF = payload.select(explode(payload("data")).alias("d"))
 
+      eventDf.writeStream.format("console").queryName("twitter").start()
+
+      spark.streaming.awaitAnyTermination
     }
 }
