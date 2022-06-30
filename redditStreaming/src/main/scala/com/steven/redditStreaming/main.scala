5c3eef14a682efd70fc5f1703820bbc44f1ba850
steven hurwitt <stevenhurwitt@gmail.com>
2022-06-30 12:56:34 -0500

    build main uberjar

--- redditStreaming/src/main/scala/com/steven/redditStreaming/main.scala
+++ redditStreaming/src/main/scala/com/steven/redditStreaming/main.scala
@@ -1,6 +1,9 @@
 package com.steven.twitter
 
+import org.apache.spark._
 import org.apache.spark.sql._
+import org.apache.spark.sql.functions._ 
+import org.apache.spark.sql.types._ 
 
 object twitter {
 
@@ -14,6 +17,7 @@ val spark: SparkSession = SparkSession.builder()
       .getOrCreate()
 
 import spark.implicits._
+// println("started spark.")
 
 val kafka_df = spark.readStream
         .format("kafka")
@@ -22,12 +26,37 @@ val kafka_df = spark.readStream
         .option("includeHeaders", "true")
         .load()
 
-val json_schema = 
-  new ArrayType(
-    new StructType()
-      .add("topic", StringType)
-      .add("subject", StringType)
-  )
+// println("read kafka.")
+
+// val json_schema = 
+//   new ArrayType(
+//     new StructType()
+//       .add("topic", StringType)
+//       .add("body", StringType)
+//   )
+
+val json_schema = new ArrayType(new StructType().add("topic", StringType))
+
+// println("created schema.")
+
+val df = kafka_df.selectExpr("CAST(body AS STRING) as json")
+            .select(from_json(col("json"), json_schema).alias("data"))
+            .select("data.*")
+
+// println("created df.")
+df.show()
+
+// df.write.format("console").option("header", "true").option("truncate", "true").load()
+df.writeStream.format("console").queryName("twitter-console").start()
+
+val target_dir = "s3:/twitter-stevenhurwitt/tweets/data/raw/twitter/"
+// df.write.format("delta").queryName("twitter-delta").option("header", "true").save(target_dir)
+print("wrote df to delta table.")
+
+// spark.streaming.awaitAnyTermination
+print("streaming...")
+
+// return(df)
 
     //   val json_schema =
     //     new ArrayType(
@@ -55,26 +84,28 @@ val json_schema =
     //         .add("metadataVersion", StringType)
     //       , true)
 
-    val json_schema = 
-        new ArrayType(
-          new StructType()
-            .add("", StringType)
-            .add("", StringType)
-            //   .add("struct", new StructType()
-            //     .add("batchId", StringType)
-            //     , true)
-            // , true)
-            // .add("metadataVersion", StringType)
-        )
+    // val json_schema = 
+    //     new ArrayType(
+    //       new StructType()
+    //         .add("", StringType)
+    //         .add("", StringType)
+    //         //   .add("struct", new StructType()
+    //         //     .add("batchId", StringType)
+    //         //     , true)
+    //         // , true)
+    //         // .add("metadataVersion", StringType)
+    //     )
 
 
-      val payload = kafka_df.selectExpr("CAST(body AS STRING) as json", "enqueuedTime", "properties").select(from_json($"json", json_schema).as("data"), col("enqueuedTime"), col("properties"))
+      // val payload = kafka_df.selectExpr("CAST(body AS STRING) as json", "enqueuedTime", "properties").select(from_json($"json", json_schema).as("data"), col("enqueuedTime"), col("properties"))
 
-      var eventDF = payload.select(explode(payload("data")).alias("d"))
+      // var eventDF = payload.select(explode(payload("data")).alias("d"))
+      
+      // eventDF = eventDF.withColumn("dt_now", )
 
-      eventDf.writeStream.format("console").queryName("twitter").start()
+      // eventDf.writeStream.format("console").queryName("twitter").start()
 
-      eventDf.write.format("delta").save("s3://twitter-stevenhurwitt/tweets/data/raw/twitter")
+      // eventDf.write.format("delta").option("header", "true").partitionBy("").save("s3://twitter-stevenhurwitt/tweets/data/raw/twitter")
 
       // spark.streaming.awaitAnyTermination
     }
