46125908c10e502d40cb0f6e3d32407335a4f2a6
steven hurwitt <stevenhurwitt@gmail.com>
2022-06-30 16:37:16 -0500

    commit

--- redditStreaming/src/main/scala/com/steven/redditStreaming/producer.scala
+++ redditStreaming/src/main/scala/com/steven/redditStreaming/producer.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql._
 import org.apache.spark.sql.functions._ 
 import org.apache.spark.sql.types._ 
 
-object twitter {
+object producer {
 
 def producer(args:Array[String]):Unit= {
 
