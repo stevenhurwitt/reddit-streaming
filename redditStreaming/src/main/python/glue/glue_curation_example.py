9eea31aae7ac30b138ac62f57cd10fee177870bf
steven hurwitt <stevenhurwitt@gmail.com>
2022-06-30 23:12:04 -0500

    set subreddit

--- redditStreaming/src/main/python/glue/glue_curation_example.py
+++ redditStreaming/src/main/python/glue/glue_curation_example.py
@@ -1,23 +1,24 @@
-import sys
-from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
+# from awsglue.transforms import *
+# from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
-from awsglue.context import GlueContext
-from awsglue.job import Job
+# from awsglue.context import GlueContext
+# from awsglue.job import Job
 from pyspark.sql.session import SparkSession
 from pyspark.sql.functions import *
 from delta import *
-from delta.tables import *
+# from delta.tables import *
+import pprint
 import boto3
+import json
+import time
+import sys
+import os
 
-args = getResolvedOptions(sys.argv, ["JOB_NAME"])
-sc = SparkContext()
-glueContext = GlueContext(sc)
-# spark = glueContext.spark_session
-job = Job(glueContext)
-job.init(args["JOB_NAME"], args)
+args = sys.argv
+print("args: {}".format(args))
+start_time = time.time()
 
-subreddit = "news"
+subreddit = os.environ["subreddit"]
 
 spark = builder = SparkSession \
   .builder \
