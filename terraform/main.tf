c26c14b6a7a1529d1868bfaebb3ccb9d55f35043
steven hurwitt <stevenhurwitt@gmail.com>
2022-07-04 01:23:49 -0500

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,10 +9,15 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+# $env:AWS_ACCESS_KEY_ID = 'T1OZE3Dt2COABio6SfgsID48gvha9OjT+8ogyV+3EKbisGR6yIsOPhJRz8ZejyvgE9+hz9IW0K3QmAIgRXAhDg=='
+# $env:AWS_SECRET_ACCESS_KEY = 'T1OZE3Dt2COABio6SfgsID48gvha9OjT+8ogyV+3EKbisGR6yIsOPhJRz8ZejyvgE9+hz9IW0K3QmAIgRXAhDg=='
+
+
 provider "aws" {
   region  = "us-east-2"
-  access_key = ""
+  access_key = "AKIA6BTEPFALMKQYDKMN"
   secret_key = ""
+  # secret_key = alphabet "q1s8hXL2MJPicEKBKDZEIhTZs+53krSxAK2PY9" double u
 }
 
 # resource "aws_instance" "reddit_server" {
@@ -24,12 +29,64 @@ provider "aws" {
 #   }
 # }
 
-resource "aws_s3_bucket_object" "subreddit" {
+# Creating a AWS secret versions for database master account (Masteraccoundb)
+# resource "aws_secretsmanager_secret_version" "sversion" {
+#   secret_id = aws_secretsmanager_secret.AWS_ACCESS_KEY_ID.id
+#   secret_string = <<EOF
+#    {
+#     "aws_client_id": "{$AWS_ACCESS_KEY_ID}",
+#     "aws_secret_key": "{$AWS_SECRET_ACCESS_KEY}"
+#    }
+# EOF
+# }
+
+# resource "aws_secretsmanager_secret_version" "ssversion" {
+#   secret_id = aws_secretsmanager_secret.AWS_SECRET_ACCESS_KEY.id
+#   secret_string = <<EOF
+#    {
+#     "aws_client_id": "{$AWS_ACCESS_KEY_ID}",
+#     "aws_secret_key": "{$AWS_SECRET_ACCESS_KEY}"
+#    }
+# EOF
+# }
+
+# resource "aws_secretsmanager" "secret" {
+#   name = "reddit-secrets-account"
+#   description = "reddit secrets account"
+#   generate_secret_string {
+#     exclude_punctuation = true
+#     exclude_lowercase = false
+#     exclude_uppercase = false
+#     exclude_digits = false
+#     exclude_space = false
+#     password_length = 20
+#     require_each_included_type = true
+#   }
+# }
+ 
+# Importing the AWS secrets created previously using arn.
+# data "aws_secretsmanager_secret" "AWS_ACCESS_KEY_ID" {
+#   arn = aws_secretsmanager_secret.AWS_ACCESS_KEY_ID.arn
+# }
+
+# locals {
+#   aws_client = data.aws_secretsmanager_secret_version.AWS_ACCESS_KEY_ID.secret_string
+# }
+
+# data "aws_secretsmanager_secret" "AWS_SECRET_ACCESS_KEY" {
+#   arn = aws_secretsmanager_secret.AWS_SECRET_ACCESS_KEY.arn
+# }
+
+# locals {
+#   aws_secret = data.aws_secretsmanager_secret_version.AWS_SECRET_ACCESS_KEY.secret_string
+# }
+
+resource "aws_s3_object" "subreddit" {
   bucket = "reddit-stevenhurwitt"
   key = "AsiansGoneWild/"
 }
 
-resource "aws_s3_bucket_object" "subreddit_clean" {
+resource "aws_s3_object" "subreddit_clean" {
     bucket  = "reddit-stevenhurwitt"
     key     =  "AsiansGoneWild_clean/"
     content_type = "application/x-directory"
