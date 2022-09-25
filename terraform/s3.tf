2affdcb34eb606fb63e5a0207515b05f8ae85bb7
steven hurwitt <stevenhurwitt@gmail.com>
2022-09-25 02:00:34 -0500

    rm interpolation only expressions terraform

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -12,51 +12,51 @@ resource "aws_s3_bucket" "reddit_streaming_stevenhurwitt" {
 
 # raw folders
 resource "aws_s3_object" "news" {
-  bucket       = "${aws_s3_bucket.reddit_stevenhurwitt.id}"
-  key          = "${var.folder_news}/"
+  bucket       = aws_s3_bucket.reddit_stevenhurwitt.id
+  key          = var.folder_news
   content_type = "application/x-directory"
 }
 
 resource "aws_s3_object" "technology" {
-  bucket       = "${aws_s3_bucket.reddit_stevenhurwitt.id}"
+  bucket       = aws_s3_bucket.reddit_stevenhurwitt.id
   key          = "${var.folder_technology}/"
   content_type = "application/x-directory"
 }
 
 resource "aws_s3_object" "worldnews" {
-  bucket       = "${aws_s3_bucket.reddit_stevenhurwitt.id}"
-  key          = "${var.folder_worldnews}/"
+  bucket       = aws_s3_bucket.reddit_stevenhurwitt.id
+  key          = var.folder_worldnews
   content_type = "application/x-directory"
 }
 
 resource "aws_s3_object" "ProgrammerHumor" {
-  bucket       = "${aws_s3_bucket.reddit_stevenhurwitt.id}"
-  key          = "${var.folder_ProgrammerHumor}/"
+  bucket       = aws_s3_bucket.reddit_stevenhurwitt.id
+  key          = var.folder_ProgrammerHumor
   content_type = "application/x-directory"
 }
 
 # clean folders
 resource "aws_s3_object" "news_clean" {
-  bucket       = "${aws_s3_bucket.reddit_stevenhurwitt.id}"
-  key          = "${var.folder_news_clean}/"
+  bucket       = aws_s3_bucket.reddit_stevenhurwitt.id
+  key          = var.folder_news_clean
   content_type = "application/x-directory"
 }
 
 resource "aws_s3_object" "technology_clean" {
-  bucket       = "${aws_s3_bucket.reddit_stevenhurwitt.id}"
-  key          = "${var.folder_technology_clean}/"
+  bucket       = aws_s3_bucket.reddit_stevenhurwitt.id
+  key          = var.folder_technology_clean
   content_type = "application/x-directory"
 }
 
 resource "aws_s3_object" "worldnews_clean" {
-  bucket       = "${aws_s3_bucket.reddit_stevenhurwitt.id}"
-  key          = "${var.folder_worldnews_clean}/"
+  bucket       = aws_s3_bucket.reddit_stevenhurwitt.id
+  key          = var.folder_worldnews_clean
   content_type = "application/x-directory"
 }
 
 resource "aws_s3_object" "ProgrammerHumor_clean" {
-  bucket       = "${aws_s3_bucket.reddit_stevenhurwitt.id}"
-  key          = "${var.folder_ProgrammerHumor_clean}/"
+  bucket       = aws_s3_bucket.reddit_stevenhurwitt.id
+  key          = var.folder_ProgrammerHumor_clean
   content_type = "application/x-directory"
 }
 
@@ -107,21 +107,21 @@ resource "aws_s3_object" "bikinibottomtwitter_clean" {
 }
 
 resource "aws_s3_object" "aws_clean" {
-  bucket       = "${aws_s3_bucket.reddit_stevenhurwitt.id}"
-  key          = "${var.folder_aws_clean}/"
+  bucket       = aws_s3_bucket.reddit_stevenhurwitt.id
+  key          = var.folder_scripts
   content_type = "application/x-directory"
 }
 
 # jar folder
 resource "aws_s3_object" "jars" {
-  bucket       = "${aws_s3_bucket.reddit_stevenhurwitt.id}"
-  key          = "${var.folder_jars}/"
+  bucket       = aws_s3_bucket.reddit_stevenhurwitt.id
+  key          = var.folder_scripts
   content_type = "application/x-directory"
 }
 
 # scripts folder
 resource "aws_s3_object" "scripts" {
-  bucket       = "${aws_s3_bucket.reddit_stevenhurwitt.id}"
-  key          = "${var.folder_scripts}/"
+  bucket       = aws_s3_bucket.reddit_stevenhurwitt.id
+  key          = var.folder_scripts
   content_type = "application/x-directory"
 }
