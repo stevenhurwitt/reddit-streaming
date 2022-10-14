0d6eef04960c5a2080ca463587949da00d7df091
steven hurwitt <stevenhurwitt@gmail.com>
2022-10-13 22:47:43 -0500

    update iam roles

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,4 +1,25 @@
 #################### s3 ####################
+resource "aws_iam_role" "s3" {
+  name = "AWSS3ServiceRoleDefault"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service":  [
+                      "s3.amazonaws.com"
+                    ]
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_iam_policy" "s3_policy" {
   name        = "s3-policy"
   description = "My test policy"
@@ -17,6 +38,11 @@ resource "aws_iam_policy" "s3_policy" {
   ]
 }
 EOT
+}
+
+resource "aws_iam_role_policy_attachment" "s3_service" {
+    role = aws_iam_role.s3.id
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSS3ServiceRole"
 }
 
 #################### glue ####################
@@ -50,7 +76,7 @@ resource "aws_iam_role_policy_attachment" "glue_service" {
 
 resource "aws_iam_role_policy" "my_s3_policy" {
   name = "my_s3_policy"
-  role = aws_iam_role.glue.id
+  role = aws_iam_role.s3.id
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -63,6 +89,29 @@ resource "aws_iam_role_policy" "my_s3_policy" {
       "Resource": [
         "arn:aws:s3:::${var.s3_bucket_name}",
         "arn:aws:s3:::${var.s3_bucket_name}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "my_glue_policy" {
+  name = "my_glue_policy"
+  role = aws_iam_role.glue.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:*",
+        "glue:*"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.reddit_streaming_stevenhurwitt.arn}",
+        "${aws_s3_bucket.reddit_streaming_stevenhurwitt.arn}/*"
       ]
     }
   ]
