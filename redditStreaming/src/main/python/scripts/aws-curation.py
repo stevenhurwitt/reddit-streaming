eb6aeb507d7318497df2336a07570ee3f4c3a894
steven hurwitt <stevenhurwitt@gmail.com>
2022-10-13 22:16:42 -0500

    update curation scripts

--- redditStreaming/src/main/python/scripts/aws-curation.py
+++ redditStreaming/src/main/python/scripts/aws-curation.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import json
+import boto3
+import pprint
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
@@ -10,7 +12,6 @@ from pyspark.sql.session import SparkSession
 from pyspark.sql.functions import *
 from delta import *
 from delta.tables import *
-import boto3
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
@@ -21,15 +22,10 @@ job.init(args["JOB_NAME"], args)
 
 subreddit = "aws"
 bucket = "reddit-streaming-stevenhurwitt"
-path = "s3a://" + bucket + "/" + subreddit
-print("aws path: {}".format(path))
 
 spark = builder = SparkSession \
   .builder \
   .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-  .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-  .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-  .config("spark.delta.logStore.class", "org.apache.spark.sql.delta.storage.S3SingleDriverLogStore") \
   .getOrCreate()
   
 # spark = configure_spark_with_delta_pip(builder).getOrCreate()
@@ -37,7 +33,7 @@ spark = builder = SparkSession \
 # .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
 # .config("spark.delta.logStore.class", "org.apache.spark.sql.delta.storage.S3SingleDriverLogStore") \
 
-df = spark.read.format("delta").option("header", True).load(path)
+df = spark.read.format("delta").option("header", True).load("s3a://" + bucket + "/" + subreddit)
 
 df = df.withColumn("approved_at_utc", col("approved_at_utc").cast("timestamp")) \
                 .withColumn("banned_at_utc", col("banned_at_utc").cast("timestamp")) \
@@ -49,20 +45,18 @@ df = df.withColumn("approved_at_utc", col("approved_at_utc").cast("timestamp")) 
                 .withColumn("day", dayofmonth(col("date"))) \
                 .dropDuplicates(subset = ["title"])
                 
-clean_path = "s3a://" + bucket + "/" + subreddit + "_clean/"
-df.write.format("delta").partitionBy("year", "month", "day").mode("overwrite").option("overwriteSchema", "true").option("header", True).save(clean_path)
+filepath = "s3a://" + bucket + "/" + subreddit + "_clean/"
+df.write.format("delta").partitionBy("year", "month", "day").mode("overwrite").option("mergeSchema", "true").option("overwriteSchema", "true").option("header", True).save(filepath)
         
-deltaTable = DeltaTable.forPath(spark, "s3a://reddit-streaming-stevenhurwitt/{}_clean".format(subreddit))
+deltaTable = DeltaTable.forPath(spark, "s3a://" + bucket + "/{}_clean".format(subreddit))
 deltaTable.vacuum(168)
 deltaTable.generate("symlink_format_manifest")
 
 athena = boto3.client('athena')
-schema = "reddit"
-
 athena.start_query_execution(
-         QueryString = "MSCK REPAIR TABLE {}.{}".format(schema,subreddit),
+         QueryString = "MSCK REPAIR TABLE reddit.{}".format(subreddit),
          ResultConfiguration = {
-             'OutputLocation': "s3://" + bucket + "/" + "_athena_results"
+             'OutputLocation': "s3://" + bucket + "/_athena_results"
          })
 
 job.commit()
--- redditStreaming/src/main/python/scripts/news-curation.py
+++ redditStreaming/src/main/python/scripts/news-curation.py
@@ -1,4 +1,8 @@
+import os
 import sys
+import json
+import boto3
+import pprint
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
@@ -8,7 +12,6 @@ from pyspark.sql.session import SparkSession
 from pyspark.sql.functions import *
 from delta import *
 from delta.tables import *
-import boto3
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
@@ -18,6 +21,7 @@ job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
 subreddit = "news"
+bucket = "reddit-streaming-stevenhurwitt"
 
 spark = builder = SparkSession \
   .builder \
@@ -29,7 +33,7 @@ spark = builder = SparkSession \
 # .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
 # .config("spark.delta.logStore.class", "org.apache.spark.sql.delta.storage.S3SingleDriverLogStore") \
 
-df = spark.read.format("delta").option("header", True).load("s3a://reddit-streaming-stevenhurwitt/" + subreddit)
+df = spark.read.format("delta").option("header", True).load("s3a://" + bucket + "/" + subreddit)
 
 df = df.withColumn("approved_at_utc", col("approved_at_utc").cast("timestamp")) \
                 .withColumn("banned_at_utc", col("banned_at_utc").cast("timestamp")) \
@@ -41,10 +45,10 @@ df = df.withColumn("approved_at_utc", col("approved_at_utc").cast("timestamp")) 
                 .withColumn("day", dayofmonth(col("date"))) \
                 .dropDuplicates(subset = ["title"])
                 
-filepath = "s3a://reddit-streaming-stevenhurwitt/" + subreddit + "_clean/"
+filepath = "s3a://" + bucket + "/" + subreddit + "_clean/"
 df.write.format("delta").partitionBy("year", "month", "day").mode("overwrite").option("mergeSchema", "true").option("overwriteSchema", "true").option("header", True).save(filepath)
         
-deltaTable = DeltaTable.forPath(spark, "s3a://reddit-streaming-stevenhurwitt/{}_clean".format(subreddit))
+deltaTable = DeltaTable.forPath(spark, "s3a://" + bucket + "/{}_clean".format(subreddit))
 deltaTable.vacuum(168)
 deltaTable.generate("symlink_format_manifest")
 
@@ -52,7 +56,7 @@ athena = boto3.client('athena')
 athena.start_query_execution(
          QueryString = "MSCK REPAIR TABLE reddit.{}".format(subreddit),
          ResultConfiguration = {
-             'OutputLocation': "s3://reddit-streaming-stevenhurwitt/_athena_results"
+             'OutputLocation': "s3://" + bucket + "/_athena_results"
          })
 
 job.commit()
