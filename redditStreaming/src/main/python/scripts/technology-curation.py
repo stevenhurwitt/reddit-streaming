c77ae21bbd0f43e2147869bee57aa47307a86fda
Steven Hurwitt <stevenhurwitt@gmail.com>
2022-12-09 21:22:29 -0600

    update curation scripts

--- redditStreaming/src/main/python/scripts/technology-curation.py
+++ redditStreaming/src/main/python/scripts/technology-curation.py
@@ -18,17 +18,26 @@ job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
 subreddit = "technology"
-postgres_host = "reddit.cq1scsldhxxi.us-east-2.rds.amazonaws.com"
-postgres_user = "postgres"
 
-secretmanager_client = boto3.client("SecretManager")
-kwargs = {"SecretId": "postgres_password"}
-secretmanager_client.get_secret_value(kwargs)
+secretmanager_client = boto3.client("secretmanager")
 
-spark = builder = SparkSession \
-  .builder \
-  .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-  .getOrCreate()
+aws_client = secretmanager_client.get_secret_value(SecretId="AWS_ACCESS_KEY_ID")
+aws_secret = secretmanager_client.get_secret_value(SecretId="AWS_SECRET_ACCESS_KEY")
+extra_jar_list = "org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0,org.apache.hadoop:hadoop-common:3.3.1,org.apache.hadoop:hadoop-aws:3.3.1,org.apache.hadoop:hadoop-client:3.3.1,io.delta:delta-core_2.12:1.2.1,org.postgresql:postgresql:42.5.0"
+
+spark = SparkSession \
+    .builder \
+    .config("spark.jars.packages", extra_jar_list) \
+    .config("spark.hadoop.fs.s3a.access.key", aws_client) \
+    .config("spark.hadoop.fs.s3a.secret.key", aws_secret) \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider') \
+    .config('spark.hadoop.fs.s3a.buffer.dir', '/opt/workspace/tmp/blocks') \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .config("spark.delta.logStore.class", "org.apache.spark.sql.delta.storage.S3SingleDriverLogStore") \
+    .enableHiveSupport() \
+    .getOrCreate()
   
 # spark = configure_spark_with_delta_pip(builder).getOrCreate()
 # .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
@@ -54,15 +63,16 @@ deltaTable = DeltaTable.forPath(spark, "s3a://reddit-streaming-stevenhurwitt/{}_
 deltaTable.vacuum(168)
 deltaTable.generate("symlink_format_manifest")
 
-connect_str = "jdbc:postgresql://{}:5432/reddit".format(postgres_host)
+postgresql = secretmanager_client.get_secret_value(SecretId="dev/postgresql")
+connect_str = "jdbc:postgresql://{}:5432/reddit".format(postgresql["postgres_host"])
 
 try:
     df.write.format("jdbc") \
         .mode("overwrite") \
         .option("url", connect_str) \
         .option("dbtable", "reddit.{}".format(subreddit)) \
-        .option("user", postgres_user) \
-        .option("password", postgres_password) \
+        .option("user", postgresql["postgres_user"]) \
+        .option("password", postgresql["postgres_password"]) \
         .option("driver", "org.postgresql.Driver") \
         .save()
 
