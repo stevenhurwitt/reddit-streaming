0f47a8acafc4a24913477bb627450205a1ed1e23
steven hurwitt <stevenhurwitt@gmail.com>
2023-01-25 10:28:01 -0600

    update spark dependent jars

--- redditStreaming/src/main/python/reddit/reddit_streaming.py
+++ redditStreaming/src/main/python/reddit/reddit_streaming.py
@@ -62,6 +62,7 @@ def init_spark(subreddit, index):
     # spark_host = "spark-master"
     aws_client = creds["aws_client"]
     aws_secret = creds["aws_secret"]
+    extra_jar_list = "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.1,org.apache.hadoop:hadoop-common:3.3.4,org.apache.hadoop:hadoop-aws:3.3.4,org.apache.hadoop:hadoop-client:3.3.4,io.delta:delta-core_2.12:2.2.0,org.postgresql:postgresql:42.5.0"
     index = 0
 
     # initialize spark session
@@ -78,7 +79,7 @@ def init_spark(subreddit, index):
                     .config("spark.eventLog.enabled", "true") \
                     .config("spark.eventLog.dir", "file:///opt/workspace/events/{}/".format(subreddit)) \
                     .config("spark.sql.debug.maxToStringFields", 1000) \
-                    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.1,org.apache.hadoop:hadoop-common:3.3.1,org.apache.hadoop:hadoop-aws:3.3.1,org.apache.hadoop:hadoop-client:3.3.1,io.delta:delta-core_2.12:1.2.1,org.postgresql:postgresql:42.5.0") \
+                    .config("spark.jars.packages", extra_jar_list) \
                     .config("spark.hadoop.fs.s3a.access.key", aws_client) \
                     .config("spark.hadoop.fs.s3a.secret.key", aws_secret) \
                     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
