ebc8f72869d0eb50ba265fa0aa03fd5436d1a58f
steven hurwitt <stevenhurwitt@gmail.com>
2023-01-29 19:58:46 -0600

    add jars, try read-only false for jupyterlab

--- redditStreaming/src/main/python/reddit/reddit_streaming.py
+++ redditStreaming/src/main/python/reddit/reddit_streaming.py
@@ -63,7 +63,7 @@ def init_spark(subreddit, index):
     aws_client = creds["aws_client"]
     aws_secret = creds["aws_secret"]
     index = 0
-    extra_jar_list = "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.1,org.apache.hadoop:hadoop-common:3.3.4,org.apache.hadoop:hadoop-aws:3.3.4,org.apache.hadoop:hadoop-client:3.3.4,io.delta:delta-core_2.12:2.2.0,org.postgresql:postgresql:42.5.0"
+    extra_jar_list = "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.1,org.apache.hadoop:hadoop-common:3.3.4,org.apache.hadoop:hadoop-aws:3.3.4,org.apache.hadoop:hadoop-client:3.3.4,io.delta:delta-core_2.12:2.2.0,org.postgresql:postgresql:42.5.0,com.google.guava:guava:31.1-jre,com.google.guava:failureaccess:1.0"
 
     # initialize spark session
     try:
