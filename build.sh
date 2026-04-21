7a0e073ad41a2f27ca2b0926d562afcee726ca83
Steven Hurwitt <stevenhurwitt@gmail.com>
2026-04-21 17:26:27 -0400

    add confluent kafka to jupyterlab env

--- build.sh
+++ build.sh
@@ -5,30 +5,30 @@ JUPYTERLAB_VERSION="4.3.4"
 # -- Building the Images using docker buildx
 
 docker buildx build \
-  -f cluster-base.Dockerfile \
+  --file cluster-base.Dockerfile \
   -t cluster-base \
   --load .
 
 docker buildx build \
   --build-arg spark_version="${SPARK_VERSION}" \
   --build-arg hadoop_version="${HADOOP_VERSION}" \
-  -f spark-base.Dockerfile \
+  --file spark-base.Dockerfile \
   -t spark-base \
   --load .
 
 docker buildx build \
-  -f spark-master.Dockerfile \
+  --file spark-master.Dockerfile \
   -t spark-master \
   --load .
 
 docker buildx build \
-  -f spark-worker.Dockerfile \
+  --file spark-worker.Dockerfile \
   -t spark-worker \
   --load .
 
 docker buildx build \
   --build-arg spark_version="${SPARK_VERSION}" \
   --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
-  -f jupyterlab.Dockerfile \
+  --file jupyterlab.Dockerfile \
   -t jupyterlab \
   --load .
